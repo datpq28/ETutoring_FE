0f84c0f00e72c298c8dab403da253988a5bdcb06
datpq28 <phamquydat2801@gmail.com>
2025-04-10 00:17:14 +0700

    Update commentdocument_service.js

--- frontend/api_service/commentdocument_service.js
+++ frontend/api_service/commentdocument_service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://etutoring-1640-1.onrender.com";
+const API_URL = "https://etutoring-be.onrender.com";
 
 // Tạo bình luận mới cho tài liệu
 export const createCommentDocument = async (documentId, commentData) => {
